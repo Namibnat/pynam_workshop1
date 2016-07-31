b2925665ebd9df50346cda71e1f896f849ef7db7
Vernon Swanepoel <frantic.naturalist@gmail.com>
2016-07-31 17:32:02 +0100

    Adding a doc string to the list function

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,6 +3,12 @@ from datetime import datetime
 
 
 def list(request):
+    """
+    This is the view for our main page
+
+    Dynamically changes the date on our
+    website every time the user refreshes the page
+    """
     thedate = datetime.now()
     content = {'date': thedate}
     return render(request, 'list.html', content)
